2fa068e72aae13a29264ebe82eda52cd78463f1f
Danykas30 <parko20a@gmail.com>
2025-06-11 11:07:40 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Postal baddies',
   description: 'Postal dud plushie',
-  generator: 'v0.dev',
+  generator: 'danykass',
 }
 
 export default function RootLayout({
