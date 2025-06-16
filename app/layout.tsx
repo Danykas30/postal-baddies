4a28fdd98db6f549bd24cfaf3918e1507b43f614
Danykas30 <parko20a@gmail.com>
2025-06-16 08:15:26 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Postal baddies',
+  title: 'Postalized: The Mission',
   description: 'Postal dud plushie',
   generator: 'danykass',
 }
